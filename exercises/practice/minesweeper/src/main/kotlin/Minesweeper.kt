9e1ce9a0e766c1f0c4888fc2ef30b6346bab1675
Gabriele Iannace <gabriele.iannace@gmail.com>
2023-03-14 11:26:11 +0100

    semi implementazione

--- exercises/practice/minesweeper/src/main/kotlin/Minesweeper.kt
+++ exercises/practice/minesweeper/src/main/kotlin/Minesweeper.kt
@@ -1,8 +1,37 @@
-data class MinesweeperBoard(val todo: Nothing) {
+data class MinesweeperBoard(val todo: List<String>) {
 
     // TODO: Implement proper constructor
+//    init {
+//        println(todo);
+//    }
+
 
     fun withNumbers(): List<String> {
-        TODO("Implement this function to complete the task")
+
+        return emptyList()
+    }
+}
+
+fun main(){
+    val strings = listOf("·*·*·", "··*··", "··*··", "·····");
+    var arr = emptyArray<Int>();
+
+    strings.mapIndexed{row, col ->
+        col.mapIndexed{r, c ->
+            if (c == '*') {
+                arr = arr.plus(r+(row * col.length));
+            }
+        }
+    }
+    println(arr.contentToString());
+
+    val flat = strings.flatMap { it.toList() }
+    println(flat)
+
+    for ((index,value) in flat.withIndex()){
+        if(value == '·'){
+            val result = arr.find{ it > 5 }
+            println(result)
+        }
     }
 }
