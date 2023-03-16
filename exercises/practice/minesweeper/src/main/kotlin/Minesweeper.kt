f2f8751717f52d46923b9a8ba7951ff2713680cf
Gabriele Iannace <gabriele.iannace@gmail.com>
2023-03-16 16:26:32 +0100

    mapIndex

--- exercises/practice/minesweeper/src/main/kotlin/Minesweeper.kt
+++ exercises/practice/minesweeper/src/main/kotlin/Minesweeper.kt
@@ -1,47 +1,27 @@
-data class MinesweeperBoard(val todo: List<String>) {
 
+data class MinesweeperBoard(private val inputBoard: List<String>) {
 
-    private var bombIndex = mutableListOf<Int>()
 
     fun withNumbers(): List<String> {
-        todo.mapIndexed { index, item ->
-            item.mapIndexed { r, c ->
-                "${if(c == '*') bombIndex.add(r+(item.length * index)) else " "}"
+        inputBoard.mapIndexed{index, value ->
+            println("index $index, value $value")
+            for((indice, valore) in value.withIndex()){
+                println("Valore --> indice $indice valore $valore")
             }
+
         }
-        return calcolaAdiacenze(todo, bombIndex)
+
+        return emptyList()
     }
 }
-
-private fun calcolaAdiacenze(strings: List<String>, bombIndex: MutableList<Int>): List<String> {
-    var result = ""
-    val joinedString = strings.joinToString(separator = "")
-
-    for((indice, cella) in joinedString.withIndex()){
-        if(cella == ' '){
-            val numeroBombe = bombIndex.filter {
-                (it == (indice - 1) && indice != 0) || (it == (indice + 1) && indice != strings[0].length - 1) ||
-                            it == (indice - strings[0].length - 1) || it == (indice - strings[0].length) || it == (indice - strings[0].length + 1) ||
-                            it == (indice + strings[0].length - 1) || it == (indice + strings[0].length) || it == (indice + strings[0].length + 1)
-                }.size
-            if (numeroBombe != 0) result += numeroBombe else result += " "
-        }
-        else{
-            result += cella
-        }
-    }
-
-    return result.chunked(strings[0].length)
-}
-
 
 fun main(){
     val inputBoard = listOf(
-        "*",
-        " ",
-        " ",
-        " ",
-        "*"
+        "  *  ",
+        "  *  ",
+        "*****",
+        "  *  ",
+        "  *  "
     )
     var istanza = MinesweeperBoard(inputBoard)
     println(istanza.withNumbers())
