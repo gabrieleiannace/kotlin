c82fd905006cc03a5d8079e4a58849ce75d62c3e
Gabriele Iannace <57633646+gabrieleiannace@users.noreply.github.com>
2023-03-21 11:27:22 +0100

    [Forth]Artimetica completata

--- exercises/practice/forth/src/main/kotlin/Forth.kt
+++ exercises/practice/forth/src/main/kotlin/Forth.kt
@@ -1,21 +1,41 @@
 class Forth {
-    private var STACK = intArrayOf()
 
     fun evaluate(vararg line: String): List<Int> {
         var evaluationStack = emptyList<Int>()
         line.map { it ->
             it.split(" ").map {
                 when (it){
-                    "+" -> evaluationStack = listOf(evaluationStack[0] + evaluationStack[1])
-                    "-" -> evaluationStack = listOf(evaluationStack[0] - evaluationStack[1])
-                    "*" -> evaluationStack = listOf(evaluationStack[0] * evaluationStack[1])
-                    "/" -> evaluationStack = listOf(evaluationStack[0] / evaluationStack[1])
-                    else -> evaluationStack = evaluationStack.plus(it.toInt());
+                    "+" ->
+                        if (evaluationStack.isEmpty()) throw CustomException("empty stack")
+                        else if(evaluationStack.size == 1) throw CustomException("only one value on the stack")
+                        else evaluationStack = listOf(evaluationStack[0] + evaluationStack[1])
+
+                    "-" ->
+                        if (evaluationStack.isEmpty()) throw CustomException("empty stack")
+                        else if(evaluationStack.size == 1) throw CustomException("only one value on the stack")
+                        else evaluationStack = listOf(evaluationStack[0] - evaluationStack[1])
+
+                    "*" ->
+                        if (evaluationStack.isEmpty()) throw CustomException("empty stack")
+                        else if(evaluationStack.size == 1) throw CustomException("only one value on the stack")
+                        else evaluationStack = listOf(evaluationStack[0] * evaluationStack[1])
+
+                    "/" ->
+                        if (evaluationStack.isEmpty()) throw CustomException("empty stack")
+                        else if(evaluationStack.size == 1) throw CustomException("only one value on the stack")
+                        else if(evaluationStack[1] == 0) throw CustomException("divide by zero")
+                        else evaluationStack = listOf(evaluationStack[0] / evaluationStack[1])
+
+                    else -> evaluationStack = evaluationStack.plus(it.toInt())
                 }
         } }
         return evaluationStack
     }
 }
+
+class CustomException(message: String) : Exception(message)
+
+
 fun main(){
     val test = Forth()
     println( test.evaluate("1 2 +"))
