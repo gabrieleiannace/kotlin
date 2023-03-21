b7a959469909082c9656a3841485802b03ee5995
Gabriele Iannace <57633646+gabrieleiannace@users.noreply.github.com>
2023-03-21 02:19:26 +0100

    Operazioni di base
    implementate addizione, sottrazione, divisione e moltiplicazione

--- exercises/practice/forth/src/main/kotlin/Forth.kt
+++ exercises/practice/forth/src/main/kotlin/Forth.kt
@@ -1,6 +1,22 @@
 class Forth {
+    private var STACK = intArrayOf()
 
     fun evaluate(vararg line: String): List<Int> {
-        TODO("Implement this to complete the task")
+        var evaluationStack = emptyList<Int>()
+        line.map { it ->
+            it.split(" ").map {
+                when (it){
+                    "+" -> evaluationStack = listOf(evaluationStack[0] + evaluationStack[1])
+                    "-" -> evaluationStack = listOf(evaluationStack[0] - evaluationStack[1])
+                    "*" -> evaluationStack = listOf(evaluationStack[0] * evaluationStack[1])
+                    "/" -> evaluationStack = listOf(evaluationStack[0] / evaluationStack[1])
+                    else -> evaluationStack = evaluationStack.plus(it.toInt());
+                }
+        } }
+        return evaluationStack
     }
+}
+fun main(){
+    val test = Forth()
+    println( test.evaluate("1 2 +"))
 }
