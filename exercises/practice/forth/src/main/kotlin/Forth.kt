08bc146f30001774ebf1124d0dcc17c628f26d25
Gabriele Iannace <57633646+gabrieleiannace@users.noreply.github.com>
2023-03-21 15:29:08 +0100

    definedWord 1

--- exercises/practice/forth/src/main/kotlin/Forth.kt
+++ exercises/practice/forth/src/main/kotlin/Forth.kt
@@ -1,42 +1,120 @@
 class Forth {
 
+    private var isWordDefinition: Boolean = false
+    private var definedWordBuffer:String = ""
+    private var definedWord: String = ""
+
     fun evaluate(vararg line: String): List<Int> {
-        var evaluationStack = emptyList<Int>()
-        line.map { it ->
-            it.split(" ").map {
+        var evaluationStack = emptyList<Int>().toMutableList()
+        line.mapIndexed { indice, it ->
+            it.split(" ").mapIndexed { index, it ->
                 when (it){
+                    definedWord -> return (evaluate(line[indice].replace(definedWord, definedWordBuffer)))
                     "+" ->
-                        if (evaluationStack.isEmpty()) throw CustomException("empty stack")
-                        else if(evaluationStack.size == 1) throw CustomException("only one value on the stack")
-                        else evaluationStack = listOf(evaluationStack[0] + evaluationStack[1])
+                        if(!isWordDefinition) {
+                            if (evaluationStack.isEmpty()) throw CustomException("empty stack")
+                            else if (evaluationStack.size == 1) throw CustomException("only one value on the stack")
+                            else evaluationStack = listOf(evaluationStack[0] + evaluationStack[1]).toMutableList()
+                        }else{
+                            definedWordBuffer = definedWordBuffer.plus("$it ")
+                        }
 
                     "-" ->
-                        if (evaluationStack.isEmpty()) throw CustomException("empty stack")
-                        else if(evaluationStack.size == 1) throw CustomException("only one value on the stack")
-                        else evaluationStack = listOf(evaluationStack[0] - evaluationStack[1])
+                        if(!isWordDefinition) {
+                            if (evaluationStack.isEmpty()) throw CustomException("empty stack")
+                            else if (evaluationStack.size == 1) throw CustomException("only one value on the stack")
+                            else evaluationStack = listOf(evaluationStack[0] - evaluationStack[1]).toMutableList()
+                        }else{
+                            definedWordBuffer = definedWordBuffer.plus("$it ")
+                        }
 
                     "*" ->
-                        if (evaluationStack.isEmpty()) throw CustomException("empty stack")
-                        else if(evaluationStack.size == 1) throw CustomException("only one value on the stack")
-                        else evaluationStack = listOf(evaluationStack[0] * evaluationStack[1])
+                        if(!isWordDefinition) {
+                            if (evaluationStack.isEmpty()) throw CustomException("empty stack")
+                            else if (evaluationStack.size == 1) throw CustomException("only one value on the stack")
+                            else evaluationStack = listOf(evaluationStack[0] * evaluationStack[1]).toMutableList()
+                        }else{
+                            definedWordBuffer = definedWordBuffer.plus("$it ")
+                        }
 
                     "/" ->
-                        if (evaluationStack.isEmpty()) throw CustomException("empty stack")
-                        else if(evaluationStack.size == 1) throw CustomException("only one value on the stack")
-                        else if(evaluationStack[1] == 0) throw CustomException("divide by zero")
-                        else evaluationStack = listOf(evaluationStack[0] / evaluationStack[1])
+                        if(!isWordDefinition) {
+                            if (evaluationStack.isEmpty() && !isWordDefinition) throw CustomException("empty stack")
+                            else if (evaluationStack.size == 1) throw CustomException("only one value on the stack")
+                            else if (evaluationStack[1] == 0) throw CustomException("divide by zero")
+                            else evaluationStack = listOf(evaluationStack[0] / evaluationStack[1]).toMutableList()
+                        } else{
+                            definedWordBuffer = definedWordBuffer.plus("$it ")
+                        }
 
-                    else -> evaluationStack = evaluationStack.plus(it.toInt())
+
+                    "dup" ->
+                        if(!isWordDefinition) {
+                            if (evaluationStack.isEmpty()) throw CustomException("empty stack")
+                            else evaluationStack =
+                                evaluationStack.plus(evaluationStack[evaluationStack.size - 1]).toMutableList()
+                        } else {
+                            definedWordBuffer = definedWordBuffer.plus("$it ")
+                        }
+
+                    "drop" ->
+                        if(!isWordDefinition) {
+                            if (evaluationStack.isEmpty()) throw CustomException("empty stack")
+                            else evaluationStack.removeLastOrNull()
+                        }else{
+                            definedWordBuffer = definedWordBuffer.plus("$it ")
+                        }
+
+                    "swap" ->
+                        if(!isWordDefinition) {
+                            if (evaluationStack.isEmpty() && !isWordDefinition) throw CustomException("empty stack")
+                            else if (evaluationStack.size == 1) throw CustomException("only one value on the stack")
+                            else {
+                                val tmp = evaluationStack[evaluationStack.size - 2]
+                                evaluationStack[evaluationStack.size - 2] = evaluationStack[evaluationStack.size - 1]
+                                evaluationStack[evaluationStack.size - 1] = tmp
+                            }
+                        }else{
+                            definedWordBuffer = definedWordBuffer.plus("$it ")
+                        }
+                    "over" ->
+                        if(!isWordDefinition) {
+                            if (evaluationStack.isEmpty()) throw CustomException("empty stack")
+                            else if (evaluationStack.size == 1) throw CustomException("only one value on the stack")
+                            else {
+                                evaluationStack =
+                                    evaluationStack.plus(evaluationStack[evaluationStack.size - 2]).toMutableList()
+                            }
+                        }else{
+                            definedWordBuffer = definedWordBuffer.plus("$it ")
+                        }
+                    ":" -> isWordDefinition = true
+                    ";" -> {
+                        isWordDefinition = false
+                        definedWordBuffer = definedWordBuffer.dropLast(1)
+                    }
+                    else ->
+                        if(!isWordDefinition) evaluationStack = evaluationStack.plus(it.toInt()).toMutableList()
+                        else {
+                            if(definedWord.isNotEmpty()){
+                                definedWordBuffer = definedWordBuffer.plus(("$it "))
+                            }else{
+                                definedWord = it
+                            }
+                        }
+
                 }
         } }
         return evaluationStack
     }
+
+
+
 }
 
 class CustomException(message: String) : Exception(message)
 
-
 fun main(){
-    val test = Forth()
-    println( test.evaluate("1 2 +"))
+    val myclass = Forth()
+    println( myclass.evaluate(": foo dup ;", ": foo dup dup ;", "1 foo" ))
 }
